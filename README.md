102c6a8b92ff4c7821b71240a0f48265555e920a
Meow <rahulniezam2002@gmail.com>
2023-02-01 04:18:40 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+### Using the Generic Interface REST of the ((OTRS)) Community Edition with PHP
+
+#### Requirements
+- ((OTRS)) Community Edition version 7
+- PHP 8.x with composer
+
+#### Prepare your ticket system
+First, download the web service configuration from [GitHub](https://raw.githubusercontent.com/OTRS/otrs/rel-6_0/development/webservices/GenericTicketConnectorREST.yml). Navigate as an admin to `Admin` => `Web Service Management` => `Add Web Service` => `Import web service`. Enter a name for the web service. I suggest to use `GenericTicketConnectorREST` because this is used in the example.
+
+#### Install example client
+Clone this repository and run `composer update` to add the [Guzzle](https://github.com/guzzle/guzzle) library:
+
+```bash
+$ git clone https://github.com/rkaldung/otrs-gi-rest-php.git php-rest-client
+$ cd php-rest-client
+$ composer update
+```
+
+#### Prepare the client
+Edit client.php and complete the baseURL and configure [FQDN](https://github.com/rkaldung/otrs-gi-rest-php/blob/master/client.php#L10), [web service name](https://github.com/rkaldung/otrs-gi-rest-php/blob/master/client.php#L11) and valid [agent credentials](https://github.com/rkaldung/otrs-gi-rest-php/blob/master/client.php#L16).
+
+#### Run your client
+Your client is ready to go and can be executed by `php client.php`
+
+#### Misc
+An introduction for the Generic Interface for the latest ((OTRS)) Community Editon is available in the [online manual](https://doc.otrs.com/doc/manual/admin/6.0/en/html/genericinterface.html).
+
+The default operations TicketCreate and TicketUpdate are not able to send a new article via e-mail. For this use case you could install the free add-on [Znuny4OTRS-GIArticleSend](https://github.com/znuny/Znuny4OTRS-GIArticleSend).
---
+++ client.php
@@ -0,0 +1,112 @@
+#!/usr/bin/env php
+<?php
+
+require_once 'vendor/autoload.php';
+use Http\Client\Common\HttpMethodsClient;
+use Http\Discovery\HttpClientDiscovery;
+use Http\Message\MessageFactory\GuzzleMessageFactory;
+
+$httpClient = new HttpMethodsClient(
+    HttpClientDiscovery::find(),
+    new GuzzleMessageFactory()
+);
+
+$FQDN = 'FIXME';
+$WebServiceName = 'GenericTicketConnectorREST';
+$BaseURL = "https://$FQDN/otrs/nph-genericinterface.pl/Webservice/$WebServiceName";
+$headers = [
+    'Accept' => 'application/json',
+    'Content-Type' => 'application/json'
+];
+$body = json_encode(
+    [
+        "UserLogin" => "FIXME",
+        "Password"  => "FIXME",
+    ]
+);
+
+/**
+ * SessionCreate
+ *
+ * http://doc.otrs.com/doc/api/otrs/6.0/Perl/Kernel/GenericInterface/Operation/Session/SessionCreate.pm.html
+ */
+$client = new \GuzzleHttp\Client();
+$response = $client->request('POST', $BaseURL."/Session", [
+    'headers' => [
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json',
+    ],
+    'verify' => true,
+    'body' => $body,
+]);
+
+$responseData = json_decode($response->getBody());
+
+if (!$responseData->SessionID) {
+    print "No SessionID returned \n";
+    exit(1);
+}
+
+$SessionID = $responseData->SessionID;
+print "Your SessionID is $SessionID\n";
+
+
+/**
+ * TicketCreate
+ *
+ * https://doc.otrs.com/doc/api/otrs/6.0/Perl/Kernel/GenericInterface/Operation/Ticket/TicketCreate.pm.html
+ */
+$attachment = file_get_contents("README.md");
+$body = json_encode([
+        'SessionID' => $SessionID,
+        'Ticket' => [
+            'Title' => 'Example ticket from PHP',
+            'Queue' => 'Postmaster',
+            'CustomerUser' => 'info@znuny.com',
+            'State' => 'new',
+            'Priority' => '3 normal',
+            'OwnerID' => 1,
+        ],
+        'Article' =>[
+            'CommunicationChannel' => 'Email',  
+            'ArticleTypeID' => 1,
+            'SenderTypeID' => 1,
+            'Subject' => 'Example',
+            'Body' => 'This is a GenericInterface example.',
+            'ContentType' => 'text/plain; charset=utf8',
+            'Charset' => 'utf8',
+            'MimeType' => 'text/plain',
+            'From' => 'info@znuny.com',
+        ],
+        'Attachment' => [
+            'Content' => base64_encode($attachment),
+            'ContentType' => 'text/plain',
+            'Filename' => 'README.md'
+        ],
+    ]
+);
+
+$response = $client->request('POST', $BaseURL."/Ticket", [
+    'headers' => [
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json',
+    ],
+    'verify' => true,
+    'body' => $body,
+]);
+
+$responseData = json_decode($response->getBody());
+
+if ( $responseData->Error ) {
+    $ErrorCode = $responseData->Error->ErrorCode;
+    $ErrorMessage = $responseData->Error->ErrorMessage;
+    print "ErrorCode $ErrorCode\n";
+    print "ErrorMessage $ErrorMessage\n";
+    exit(1);
+}
+
+$TicketNumber = $responseData->TicketNumber;
+$TicketID = $response->body->TicketID;
+foreach($TicketIDs as $TicketID) {
+    print "https://$FQDN/otrs/index.pl?Action=AgentTicketZoom;TicketID=$TicketID \n";
+} 
